0a29390376ef882b37521067209bea028f56693e
qyy <yyqian@che300.com>
2020-12-03 10:23:00 +0800

    自定义loader和plugin

--- loader/markdown-loader.js
+++ loader/markdown-loader.js
@@ -2,9 +2,9 @@ const marked = require("marked");
 const loaderUtils = require("loader-utils");
 
 module.exports = function (source) {
-    console.log(source);
+    // console.log(source);
     const options = loaderUtils.getOptions(this);
-    console.log(111, options);
+    // console.log(111, options);
 
     // 定义⼀个异步处理，告诉webpack,这个loader⾥有异步事件,在⾥⾯调⽤下这个异步
     // 使⽤this.asycn来处理，他会返回this.callback
--- myPlugin.js
+++ myPlugin.js
@@ -3,11 +3,25 @@ class MyPlugin {
         console.log("MyPlugin 启动");
         // console.log(compiler);
 
-        // compiler.hooks.emit.tap("MyPlugin", (complation) => {
-        //     for (const name in complation.assets) {
-        //         console.log(name);
-        //     }
-        // });
+        compiler.hooks.emit.tap("MyPlugin", (complation) => {
+            // console.log(complation.assets);
+            for (const name in complation.assets) {
+                // console.log(complation.assets[name].source());
+
+                if (name.endsWith(".js")) {
+                    const contents = JSON.stringify(
+                        complation.assets[name].source()
+                    );
+                    const withoutComments = JSON.parse(
+                        contents.replace("window", "global")
+                    );
+                    complation.assets[name] = {
+                        source: () => withoutComments,
+                        size: () => withoutComments.length,
+                    };
+                }
+            }
+        });
     }
 }
 
